f4e8ba87dc5e3794bc92142bbfe228ede3a72be6
moazrana <muaazmehmood@gmail.com>
2025-08-08 18:05:16 +0500

    1p4

--- server/src/seeders/seeder.module.ts
+++ server/src/seeders/seeder.module.ts
@@ -18,7 +18,7 @@ import { StaffSeedCommand } from './staff-seed.command';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/cbms'),
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cbms'),
     MongooseModule.forFeature([
       { name: Role.name, schema: RoleSchema },
       { name: User.name, schema: UserSchema },
